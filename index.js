489ca08c9f088738c5b2b220be3794970ee9303d
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-03-27 21:37:09 -0300

    'api'

--- index.js
+++ index.js
@@ -31,7 +31,23 @@ var DB = {
 app.get("/games",(req, res) => {
     res.statusCode = 200;
     res.json(DB.games);
-})
+});
+
+app.get("/game/:id",(req, res) => {
+    if(isNaN(req.params.id)) {
+        res.sendStatus(400);
+    } else {
+        var id = parseInt(req.params.id);
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined) {
+            res.statusCode = 200;
+            res.json(game);
+        } else {
+            res.sendStatus(404);
+        }
+    }
+});
 
 app.listen(3000,() => {
     console.log("API RODANDO");
